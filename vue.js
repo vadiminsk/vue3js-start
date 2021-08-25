30b160420c302a2aa2099d4bc558d2eba3bd0a18
Vadim <vadiminsk90@gmail.com>
2021-08-25 19:19:51 +0300

    create shopping list app

--- vue.js
+++ vue.js
@@ -1,25 +1,35 @@
-const TodoList = {
+const shoppingList = Vue.createApp({
   data() {
     return {
-      javascriptFrameworks: [
-        { id: 1, text: 'Vue' },
-        { id: 2, text: 'React' },
-        { id: 3, text: 'Angular' },
+      header: 'Shopping List App',
+      newItem: '',
+      newItemPriority: false,
+      editing: false,
+      purchased: false,
+      items: [
+        { id: 1, title: '10 hats' },
+        { id: 2, title: '5 cups' },
+        { id: 3, title: '2 games' },
       ],
     };
   },
-  // methods: {
-  //   toggleText() {
-  //     this.seen = !this.seen;
-  //   },
-  // },
-};
-
-const app = Vue.createApp(TodoList);
-
-app.component('todo-item', {
-  props: ['todo'],
-  template: '<li>{{todo.text}}</li>',
-});
-
-app.mount('#todo');
+  methods: {
+    saveItem() {
+      this.items.push({
+        id: this.items.length + 1,
+        title: this.newItem,
+        highPriority: this.newItemPriority,
+      });
+      this.newItem = '';
+      this.newItemPriority = false;
+    },
+    doEdit() {
+      this.editing = !this.editing;
+      this.newItem = '';
+      this.newItemPriority = false;
+    },
+    togglePurchased(item) {
+      item.purchased = !item.purchased;
+    },
+  },
+}).mount('#shopping-list');

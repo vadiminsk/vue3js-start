2e7b231bb056ed6aacd82b47fed9fd52ea90e029
Vadim <vadiminsk90@gmail.com>
2021-08-26 10:03:53 +0300

    create components

--- vue.js
+++ vue.js
@@ -1,35 +1,23 @@
-const shoppingList = Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
-      header: 'Shopping List App',
-      newItem: '',
-      newItemPriority: false,
-      editing: false,
-      purchased: false,
-      items: [
-        { id: 1, title: '10 hats' },
-        { id: 2, title: '5 cups' },
-        { id: 3, title: '2 games' },
+      plans: [
+        { id: 1, name: 'Create App' },
+        { id: 2, name: 'Buld VueJS App' },
+        { id: 3, name: 'Rebuild the project' },
       ],
     };
   },
-  methods: {
-    saveItem() {
-      this.items.push({
-        id: this.items.length + 1,
-        title: this.newItem,
-        highPriority: this.newItemPriority,
-      });
-      this.newItem = '';
-      this.newItemPriority = false;
-    },
-    doEdit() {
-      this.editing = !this.editing;
-      this.newItem = '';
-      this.newItemPriority = false;
-    },
-    togglePurchased(item) {
-      item.purchased = !item.purchased;
-    },
+});
+
+app.component('plan', {
+  props: {
+    name: String,
   },
-}).mount('#shopping-list');
+
+  template: `<div>
+        <span>{{name}}</span>
+      </div>`,
+});
+
+app.mount('#app');

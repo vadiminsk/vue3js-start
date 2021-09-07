000062ff62f95754a73c240fd58a06bea871bbc9
Vadim <vadiminsk90@gmail.com>
2021-09-07 13:41:10 +0300

    perspective playground

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-const vm = Vue.createApp({
+Vue.createApp({
   data() {
     return {
-      mode: 1,
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
     };
+  },
+
+  methods: {
+    reset() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+
+    copy() {
+      const el = document.createElement('textarea');
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      el.value = `transform: ${this.box.transform}`;
+      document.body.appendChild(el);
+      el.select();
+
+      navigator.clipboard
+        .readText(el.value)
+        .then(() => {
+          console.log(el.value);
+        })
+        .catch((error) => {
+          alert(`Copy failed! ${error}`);
+        });
+
+      // document.body.removeChild(el);
+    },
+  },
+
+  computed: {
+    box() {
+      return {
+        transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)`,
+      };
+    },
   },
 }).mount('#app');

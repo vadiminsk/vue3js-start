cdca5b00afd15e6d6cb0f0af6928229b689ff4c0
Vadim <vadiminsk90@gmail.com>
2021-09-07 09:54:48 +0300

    Binding Classes

--- script.js
+++ script.js
@@ -1,45 +1,16 @@
 const vm = Vue.createApp({
   data() {
     return {
-      firstName: 'Vadim',
-      middleName: '',
-      lastName: 'Gadjimuradov',
-      url: 'https://www.google.com/',
-      raw_url: '<a href="https://www.google.com/" target="_blank">Google</a>',
-      age: 20,
+      isPurple: false,
+      selectedColor: '',
     };
   },
 
-  methods: {
-    increment() {
-      this.age++;
-    },
-
-    decrement() {
-      this.age--;
-    },
-
-    updateLastName(msg, event) {
-      console.log(msg);
-      this.lastName = event.target.value;
-    },
-
-    updateMiddleName(event) {
-      this.middleName = event.target.value;
-    },
-  },
-
   computed: {
-    fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`;
-    },
-  },
-
-  watch: {
-    age(newVal, oldVal) {
-      setTimeout(() => {
-        this.age = 21;
-      }, 2000);
+    circleClasses() {
+      return {
+        purple: this.isPurple,
+      };
     },
   },
 }).mount('#app');

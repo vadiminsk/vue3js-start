9577ca518709bb480f8713a761b4f7677e10a3f3
Vadim <vadiminsk90@gmail.com>
2021-09-08 12:50:41 +0300

    Props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
-<template><Greeting /> </template>
+<template>
+  <div class="wrapper">
+    <h3>Hey!</h3>
+    <button type="button" @click="age++">Update the age</button>
+    <greeting :age="age"></greeting>
+    <user :age="age"></user>
+  </div>
+</template>
 
 <script>
 import Greeting from './components/Greeting.vue';
+import User from './components/User.vue';
 
 export default {
   name: 'App',
   components: {
     Greeting,
+    User,
+  },
+
+  data() {
+    return {
+      age: 20,
+    };
   },
 };
 </script>
--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,10 +1,16 @@
 <template>
-  <p>{{ msg }}</p>
+  <p v-if="age > 25">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad rem dolore id.
+    Corporis earum odio, totam hic provident nam, minus asperiores natus alias,
+    laboriosam magni quidem. Fugiat itaque laboriosam perspiciatis!
+  </p>
+  <p v-else>{{ msg }}</p>
 </template>
 
 <script>
 export default {
   name: 'Greeting',
+  props: ['age'],
   data() {
     return {
       msg: 'Hello World',
